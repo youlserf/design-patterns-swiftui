ce95be5a23ec9c7714c22a17eec2e541a288a4fb
youlserf <youlserf.cardenas@tecsup.edu.pe>
2023-05-10 16:11:44 -0700

    Initial Commit

---
+++ design-patterns/design_patternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  design_patternsApp.swift
+//  design-patterns
+//
+//  Created by youlserf on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct design_patternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
